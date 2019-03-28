3298256f1e62c6a2697bb2c2d2416a4b4b4bde67
lopl233 <lopl233@o2.pl>
2019-03-28 08:54:37 +0100

    added base img's

--- python/test.py
+++ python/test.py
@@ -1,1 +1,8 @@
-print("smt")
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('img/standardleaves/1.jpg')
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
