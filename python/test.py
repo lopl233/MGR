3dd8956d048aeb8960bf90b62b5d145821c40e04
lopl233 <lopl233@o2.pl>
2019-04-25 09:14:44 +0200

    select one countour

--- python/test.py
+++ python/test.py
@@ -1,19 +1,23 @@
 
 import numpy as np
 import cv2 as cv
-im = cv.imread('img/standardleaves/6.jpg')
+im = cv.imread('img/standardleaves/16.jpg')
 
-im = cv.blur(im, (8, 8))
+im = cv.blur(im, (20, 20))
 
 imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-ret, thresh = cv.threshold(imgray, 150, 255, 0)
+ret, thresh = cv.threshold(imgray, 200, 255, 0)
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE,)
-blank_image = np.zeros((imgray.shape[0],imgray.shape[0],3), np.uint8)
+blank_image = np.zeros((imgray.shape[0],imgray.shape[1],3), np.uint8)
+
+contours_len = [len(x) for x in contours]
+contours = [x for x in contours if len(x) == max(contours_len) ]
+
 img = cv.drawContours(imgray, contours, -1, (0,255,0), 3)
 img = cv.resize(img, (0,0), fx=0.5, fy=0.5)
+
 cv.imshow('image',img)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
 
-print(len(contours))
